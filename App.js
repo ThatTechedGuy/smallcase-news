20b2105be8653b492adbce38e682ae34ce138722
ThatTechedGuy <vvishalgupta5@gmail.com>
2020-07-03 20:46:58 +0530

    one mode complete

--- App.js
+++ App.js
@@ -3,32 +3,56 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import Switcher from "./src/components/Switcher";
+import { Provider as PaperProvider, DefaultTheme } from "react-native-paper";
+
 
 const Stack = createStackNavigator();
 
-const App = () => (
-  <Stack.Navigator initialRouteName="Home">
-    <Stack.Screen
-      name="Home"
-      component={HomeScreen}
-      options={{
-        headerTitle: "News",
-        headerTitleStyle: {
-          color: "#fff",
-          fontSize: 40
-        },
-        headerRight: () => <Switcher />,
-        headerStyle: {
-          backgroundColor: "#7a44cf",
-          height: 120,
-        },
-      }}
-    ></Stack.Screen>
-  </Stack.Navigator>
-);
+const App = () => {
+  return (
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerTitle: "News",
+          headerTitleStyle: {
+            color: "#fff",
+            fontSize: 40,
+          },
+          headerRight: () => <Switcher />,
+          headerStyle: {
+            backgroundColor: "#7a44cf",
+            height: 120,
+          },
+        }}
+      ></Stack.Screen>
+    </Stack.Navigator>
+  );
+};
 
-export default () => (
-  <NavigationContainer>
-    <App />
-  </NavigationContainer>
-);
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+};
+
+export default () => {
+  return (
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <App />
+      </NavigationContainer>
+    </PaperProvider>
+  );
+};
+
+// dark: true,
+// mode: "exact",
+// colors: {
+//   ...DefaultTheme.colors,
+//   primary: "#7a44cf",
+//   accent: "#f1c40f",
+//   background: '#333333',
+//   surface: '#121212',
+//   text: '#fff'
+// },
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,25 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList, ActivityIndicator, Image } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
+import { Button, Text, ActivityIndicator } from "react-native-paper";
+
 import api from "../api/newsApi";
 import ListCard from "../components/Card";
 
 const HomeScreen = () => {
-  const [data, setData] = useState([]);
+  const [news, setNews] = useState([]);
   const [offset, setOffset] = useState(0);
-  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
-      console.log("FETCHING DATA");
-      setLoading(true);
-      const response = await api.get("/getNews", { params: { count: 20, offset } });
+      const response = await api.get("/getNews", { params: { count: 20, offset: offset * 20 } });
       const { data } = response.data;
       const requiredData = data.map((item) => {
         const { _id, headline, summary, createdAt, imageUrl } = item;
         return { _id, headline, summary, createdAt, imageUrl };
       });
-      setLoading(false);
-      setData(requiredData);
+      setNews(news.concat(requiredData));
     };
 
     fetchData();
@@ -27,15 +25,14 @@ const HomeScreen = () => {
 
   return (
     <View style={styles.container}>
-      {loading ? (
-        <ActivityIndicator size="large" />
-      ) : (
-        <FlatList
-          data={data}
-          keyExtractor={(item) => item._id}
-          renderItem={({ item }) => <ListCard item={item} />}
-        />
-      )}
+      <FlatList
+        data={news}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <ListCard item={item} />}
+        onEndReachedThreshold={0.1}
+        onEndReached={() => setOffset(offset + 1)}
+        ListFooterComponent={() => <ActivityIndicator size="small" />}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -43,7 +40,7 @@ const HomeScreen = () => {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 5,
+    paddingTop: 5,
   },
   row: {
     flex: 0.5,
