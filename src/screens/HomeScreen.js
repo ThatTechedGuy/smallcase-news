a2faae0740a17aff8c3ac7b3b7e9be116af4f0fb
ThatTechedGuy <vvishalgupta5@gmail.com>
2020-07-05 21:03:21 +0530

    refreshing bug fixed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,29 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text } from "react-native";
-import useNews from "../hooks/useNews";
+import { StyleSheet, View } from "react-native";
 import { COMFY } from "../constants";
 
 import ListView from "../components/ListView";
 import ComfyCard from "../components/ComfyCard";
 import CompactCard from "../components/CompactCard";
 import ErrorText from "../components/ErrorText";
-import { set } from "react-native-reanimated";
+
+import {Context as ViewContext} from './../context/ViewContext';
 
 const HomeScreen = ({ navigation }) => {
   const [offset, setOffset] = useState(0);
-  /* returns data and viewtype in which it will be displayed. */
-  const [news, viewType, error] = useNews(offset);
+  /* 
+     returns data and viewtype in which it will be displayed in the current state 
+     along with some actions to dispatch.
+  */
+  const {
+    state: { news, viewType, error },
+    fetchNews,
+  } = useContext(ViewContext);
   /* On reaching the list end, more data is fetched. */
   const handleListEnd = () => setOffset(offset + 1);
   /* Navigate to the details screen on button press. */
-  const handleNavigation = (item) => navigation.navigate("Detail", { id: item._id });
+  const handleNavigation = (item) =>
+    navigation.navigate("Detail", { id: item._id });
   /* Render the particular cards based on the mode. */
-  const renderComfy = (item) => <ComfyCard item={item} handleNavigation={handleNavigation} />;
-  const renderCompact = (item) => <CompactCard item={item} handleNavigation={handleNavigation} />;
+  const renderComfy = (item) => (
+    <ComfyCard item={item} handleNavigation={handleNavigation} />
+  );
+  const renderCompact = (item) => (
+    <CompactCard item={item} handleNavigation={handleNavigation} />
+  );
+
+  React.useEffect(() => {
+    fetchNews(offset);
+  }, [offset]);
 
   if (error !== "") {
-    return <ErrorText errorText={error} handlePress={() => setOffset(0)} />;
+    if (offset !== 0)
+      return <ErrorText errorText={error} handlePress={() => setOffset(0)} />;
+    else if (offset === 0) {
+      return <ErrorText errorText={error} handlePress={() => fetchNews(0)} />;
+    }
   }
 
   return (
